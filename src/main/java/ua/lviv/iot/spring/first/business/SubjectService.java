38155eef5442525d26873cf8ef52f707a9fd5ced
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-06 21:12:43 +0300

    Created service class for Subject

---
+++ src/main/java/ua/lviv/iot/spring/first/business/SubjectService.java
@@ -0,0 +1,19 @@
+package ua.lviv.iot.spring.first.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.lviv.iot.spring.first.dataaccess.SubjectRepository;
+import ua.lviv.iot.spring.first.rest.model.Subject;
+
+@Service
+public class SubjectService {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    public List<Subject> getSubjects() {
+        return subjectRepository.findAll();
+    }
+}
