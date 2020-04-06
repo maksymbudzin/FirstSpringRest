801aad571994ab1a100cd31043bb7246a9c067cb
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-06 21:08:25 +0300

    Formatted code and added new method to find all students with certain name

--- src/main/java/ua/lviv/iot/spring/first/business/StudentService.java
+++ src/main/java/ua/lviv/iot/spring/first/business/StudentService.java
@@ -1,8 +1,6 @@
 package ua.lviv.iot.spring.first.business;
 
 import java.util.List;
-
-import org.apache.catalina.filters.AddDefaultCharsetFilter.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,34 +13,39 @@ public class StudentService {
 
     @Autowired
     private StudentRepository studentRepository;
-    
+
     public Student createStudent(Student student) {
         return studentRepository.save(student);
     }
-    
+
     public HttpStatus deleteStudent(Integer id) {
-        if(studentRepository.existsById(id)) {
+        if (studentRepository.existsById(id)) {
             studentRepository.deleteById(id);
             return HttpStatus.OK;
         } else {
             return HttpStatus.NOT_FOUND;
         }
     }
+
     public Student getStudent(Integer id) {
         return studentRepository.findById(id).get();
     }
-    
+
     public List<Student> getStudents() {
         return studentRepository.findAll();
     }
-    
+
     public ResponseEntity<Student> updateStudent(Student student, Integer id) {
-        if(studentRepository.existsById(id)) {
+        if (studentRepository.existsById(id)) {
             student.setId(id);
             Student newStudent = studentRepository.save(student);
-            return new ResponseEntity<Student>(newStudent,HttpStatus.OK);
+            return new ResponseEntity<Student>(newStudent, HttpStatus.OK);
         } else {
             return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    public List<Student> getAllByFirstName(String firstName) {
+        return studentRepository.findAllByFirstName(firstName);
     }
 }
