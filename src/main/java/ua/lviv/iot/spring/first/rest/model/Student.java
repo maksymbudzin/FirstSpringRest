aa7a650c34f4ca16316b0250dfc943d6c264596b
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-06 21:06:18 +0300

    Created connection many to many

--- src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/model/Student.java
@@ -1,16 +1,20 @@
 package ua.lviv.iot.spring.first.rest.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-
+import javax.persistence.NamedNativeQuery;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
+@NamedNativeQuery(name = "Student.findBestStudent", query = "select from student where id =1")
 public class Student {
 
     @Id
@@ -18,21 +22,26 @@ public class Student {
     private Integer id;
     private String firstName;
     private String lastName;
-    
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "group_id")
     @JsonIgnoreProperties("students")
     private Group group_id;
-    
+
+    @ManyToMany(mappedBy = "students")
+    @JsonIgnoreProperties("students")
+    private Set<Subject> subjects;
+
     public Student() {
-        
+
     }
+
     public Student(String firstName, String lastName) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    
+
     public Integer getId() {
         return id;
     }
@@ -44,19 +53,32 @@ public class Student {
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public Group getGroup_id() {
         return group_id;
     }
+
     public void setGroup_id(Group group_id) {
         this.group_id = group_id;
-    } 
+    }
+
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
+    }
 }
