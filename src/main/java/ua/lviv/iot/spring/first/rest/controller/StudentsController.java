8e427339e0f7f0f3fd80c55b2676f9d04b6005a2
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-04-06 21:09:33 +0300

    Formatted code and modified get method to find students by first name

--- src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
+++ src/main/java/ua/lviv/iot/spring/first/rest/controller/StudentsController.java
@@ -1,9 +1,6 @@
 package ua.lviv.iot.spring.first.rest.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,8 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import ua.lviv.iot.spring.first.business.StudentService;
 import ua.lviv.iot.spring.first.rest.model.Student;
 
@@ -24,15 +21,15 @@ import ua.lviv.iot.spring.first.rest.model.Student;
 @RestController
 public class StudentsController {
 
-    private Map<Integer, Student> students = new HashMap<>();
-    private AtomicInteger idCounter = new AtomicInteger();
-    
     @Autowired
     private StudentService studentService;
 
-    @GetMapping
-    public List<Student> getStudents() {
-        return studentService.getStudents();
+    @GetMapping()
+    public List<Student> getStudents(final @RequestParam(name = "firstName", required = false) String firstName) {
+        if (firstName == null) {
+            return studentService.getStudents();
+        }
+        return studentService.getAllByFirstName(firstName);
     }
 
     @GetMapping(path = "/{id}")
@@ -47,14 +44,13 @@ public class StudentsController {
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Student> deleteStudent(@PathVariable("id") Integer studentId) {
-//        HttpStatus status = students.remove(studentId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
-//        return ResponseEntity.status(status).build();
-          HttpStatus status = studentService.deleteStudent(studentId);
-          return ResponseEntity.status(status).build();
+        HttpStatus status = studentService.deleteStudent(studentId);
+        return ResponseEntity.status(status).build();
     }
 
     @PutMapping(path = "/{id}")
-    public ResponseEntity<Student> updateStudent(final @PathVariable("id") Integer studentId, final @RequestBody Student student) {
-      return studentService.updateStudent(student, studentId);
+    public ResponseEntity<Student> updateStudent(final @PathVariable("id") Integer studentId,
+            final @RequestBody Student student) {
+        return studentService.updateStudent(student, studentId);
     }
 }
